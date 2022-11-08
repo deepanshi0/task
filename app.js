d950b42b1a1cd2cb9e1da77f272b702a1ba13771
Deepanshi Singh <89002908+deepanshi0@users.noreply.github.com>
2022-11-08 16:52:02 +0530

    Add files via upload

---
+++ app.js
@@ -0,0 +1,41 @@
+const express = require("express");
+const path = require("path");
+// const fs = require("fs");
+const app = express();
+const bodyparser = require("body-parser");
+
+app.use('/static', express.static('static')) // For serving static files
+
+app.use(express.urlencoded())
+
+
+app.get('/', (req, res)=>{
+    const params = {}
+    res.status(200).render('index.html', params);
+})
+
+
+app.get('/index1', (req, res)=>{
+    const params = {}
+    res.status(200).render('index1.html', params);
+})
+app.get('/index2', (req, res)=>{
+    const params = {}
+    res.status(200).render('index2.html', params);
+})
+app.get('/index3', (req, res)=>{
+    const params = {}
+    res.status(200).render('index3.html', params);
+})
+app.get('/index4', (req, res)=>{
+    const params = {}
+    res.status(200).render('index4.html', params);
+})
+app.get('/index5', (req, res)=>{
+    const params = {}
+    res.status(200).render('index5.html', params);
+})
+
+app.listen(port, ()=>{
+    console.log(`The application started successfully on port 80`);
+});
